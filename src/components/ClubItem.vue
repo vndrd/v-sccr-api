014347d02dece7651e4faf7456e4709c70c6a4e3
vndrd <ivangddzzz@gmail.com>
2020-05-17 16:21:25 -0400

    logos de clubes

---
+++ src/components/ClubItem.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="item">
+        <h3>
+            {{team.name}}
+        </h3>
+        <img :src="team.crestUrl" alt="" height="100px">
+    </div>    
+</template>
+<script>
+export default {
+    props: ['team'],
+    name: 'ClubItem'
+}
+</script>
+<style lang="scss" scoped>
+.item {
+    background: #bbb;
+    overflow: hidden;
+    padding: 15px;
+    height: 100px;
+    position:relative;
+    cursor: pointer;
+}
+img{
+    transition: .3s all ease-in-out;
+    // background: yellow;
+}
+.item:hover img {
+    -webkit-transform: scale(1.2);
+      -ms-transform: scale(1.2);
+          transform: scale(1.2);
+}
+.item:hover h3{
+    opacity: 1;
+}
+h3 {
+    font-size: 1.1rem;
+    background-color: #000;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    font-weight: 200;
+    width: 100%;
+    color: #aaa;
+    max-height: 50px;
+    -moz-transform: translateY(20px);
+    opacity: 0;
+    transition: .3s all ease-in-out;
+    z-index: 99;;
+}
+</style>
--- src/views/Competition.vue
+++ src/views/Competition.vue
@@ -1,23 +1,25 @@
 <template>
   <div>
-      <div v-if="loaded">
+      <div v-if="loaded" >
         <h1>{{league.name}}</h1>    
         <h3>{{league.area.name}}</h3>    
-        <h3 v-if="league.winner">Campeón: {{league.winner.name}}</h3>    
-        <p v-if="league.winner">img: {{league.winner.crestUrl}}</p>
-        <ul>
-            <li v-for="team in league.teams" :key="team.id">
-                {{team.name}}
-            </li>
-        </ul>
+        <h3 v-if="league.winner">Campeón: {{league.winner.name}}</h3>
+        <p v-if="league.winner">
+            img: {{league.winner.crestUrl}}
+        </p>
+        <div class="container-club">
+            <ClubItem v-for="team in league.teams" :key="team.id" :team="team"/>
+        </div>
       </div>
   </div>
 </template>
 <script>
 //<button @click="getSingleLeague">qwe</button>
+import ClubItem from '@/components/ClubItem'
 import {mapActions,mapGetters} from 'vuex'
 export default {
     props: ['id','urr','idd'],
+    components: {ClubItem},
     data(){
         return {
             loaded: false
@@ -35,24 +37,14 @@ export default {
     computed: {
         ...mapGetters(['getSingleLeague']),
         league: function(){
-            if(
-                Object.prototype.hasOwnProperty.call(this.getSingleLeague, 'competition')
-            ){
-                
-                console.log("sheldon")
-                console.log(this.getSingleLeague.season)
+            if(Object.prototype.hasOwnProperty.call(this.getSingleLeague, 'competition')){
                return {
                    ...this.getSingleLeague.competition,
                    winner : this.getSingleLeague.season.winner,
                    teams: this.getSingleLeague.teams
                }
             }
-            return {
-                area: {},
-                season: {},
-                teams: [],
-                winner: {}
-                }
+            return {area: {}, season: {}, teams: [], winner: {}}
         }
     },
     beforeDestroy(){
@@ -61,18 +53,13 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-ul {
-    margin: 0;
-    padding: 0;
-    text-align: center;
-    a {
-       text-decoration: none;
-       color: #666;
-        padding:10px 1px;
-        display: block;
-        margin: 5px auto;
-        width: 200px;
-        background: #ccc;
-    }
+.container-club {
+    display: grid;
+    grid-template-columns: repeat(10,1fr);
+    row-gap: 5px;
+    column-gap: 5px;
+}
+.grid-item {
+    
 }
 </style>
