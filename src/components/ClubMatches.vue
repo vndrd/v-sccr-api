a1b66bf586796b373b362631a1ba64352fcbf855
vndrd <ivangddzzz@gmail.com>
2020-05-20 18:48:43 -0400

    color resultado

--- src/components/ClubMatches.vue
+++ src/components/ClubMatches.vue
@@ -3,13 +3,16 @@
         <table>
             <tbody>
                 <tr v-for="match in matches" :key="match.id">
-                    <td>{{match.matchday}}</td> 
-                    <!-- <td v-if="getClub.id===match.homeTeam.id"> Q Q {{match.homeTeam.name}} </td> -->
-                    <!-- <td v-else> {{match.homeTeam.name}} </td> -->
-                    <td> {{match.homeTeam.name}} </td>
-                    <td>{{match.score.fullTime.homeTeam}}</td>
-                    <td>{{match.score.fullTime.awayTeam}}</td>
-                    <td> {{match.awayTeam.name}}</td>
+                    <td :class="verificar(match.homeTeam.id)? 'current-club':''"> 
+                        {{match.homeTeam.name}} 
+                    </td>
+                    <td :class="scoreColor(match)">
+                        {{match.score.fullTime.homeTeam}} -
+                        {{match.score.fullTime.awayTeam}}
+                    </td>
+                    <td :class="verificar(match.awayTeam.id)? 'current-club':''"> 
+                         {{match.awayTeam.name}}
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -22,9 +25,41 @@ export default {
     props: ['matches'],
     computed: {
         ...mapGetters(['getClub'])
+    },
+    methods :{
+        verificar: function(id){
+            return this.getClub.id === id
+        },
+        scoreColor: function({homeTeam,score}){
+            let devolverClase = function(actual,adversario){
+                if(actual > adversario)  return 'color-victoria'
+                if(actual < adversario)  return 'color-derrota'
+                if(actual === adversario)  return 'color-empate'
+            }
+            let { fullTime } = score
+            if( fullTime.homeTeam === null ) return ''
+            if( this.getClub.id === homeTeam.id ){
+                return devolverClase(fullTime.homeTeam,fullTime.awayTeam)
+            }else{
+                return devolverClase(fullTime.awayTeam,fullTime.homeTeam)
+            }
+        }
     }
+
 }
 </script>
 <style lang="scss" scoped>
-
+.current-club{
+    font-weight: 800;
+}
+table {
+    border-collapse: separate;
+    border-spacing: 0 10px;
+}
+.color-empate{background: yellow;}
+.color-victoria{background: green;}
+.color-derrota{background: red;}
+tr td:nth-child(2) {
+    width: 50px;
+}
 </style>
